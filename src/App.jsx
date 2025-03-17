49c5d01c965238369bce7d59bae9da5a5d1818d1
Everest Paudel <paudeleverest09@gmail.com>
2025-03-17 15:28:10 +0545

    Feat : Add video streaming functionality and improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,29 @@ import  { Toaster } from 'react-hot-toast';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [videoId,setVideoId]= useState("f5bc32b3-9412-40f1-8cdb-6dfd2ff6edbc")
 
   return (
  <>
     <Toaster/>
- <div className='flex flex-col items-center space-y-5 justify-center py-9'>
-  <h1 className="text-5xl font-extrabold text-gray-800 dark:text-gray-100 ">Welcome to video Streaming App</h1>
+    <div className='mt-10  flex justify-center'>
+    <h1 className="text-5xl font-extrabold text-gray-800 dark:text-gray-100 ">Welcome to video Streaming App</h1>
+
+    </div>
+
+    <div className='flex  justify-around mt-10 '>
+
+    <div>
+
+<video src={`http://localhost:8080/api/v1/videos/stream/${videoId}`} controls></video>
+</div>
+    <div className='flex flex-col items-center space-y-5 justify-center py-9'>
+ 
   <VideoUpload/>
  </div>
+
+    </div>
+
  </>
   )
 }
